9feedacced2d4137428da1dea3921ed09d4fda2f
dsm122n <90348806+dsm122n@users.noreply.github.com>
2023-05-06 03:54:53 +0000

    corrigien2

--- graficos_concentraciones.R
+++ graficos_concentraciones.R
@@ -18,6 +18,7 @@ asc_graph <- ggplot() +
     # if you want to add standard deviation adjusted to 95% confidence interval
     # stat_summary(data = asc, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, fun.args=list(mult=1.96), geom = "errorbar", width = 4) +
     # set colors
+    scale_x_continuous(breaks = seq(0, 180, 30)) +
     scale_color_manual(values = c("#5c5c5c",  "#6c0000", "#04176b"), 
                         labels = c("Placebo", "CAT 1", "CAT 2")) +
     scale_shape_manual(values = c(17, 16, 15),
@@ -31,6 +32,22 @@ asc_graph <- ggplot() +
         plot.title = element_text(size = 12))
 asc_graph
 
+#same graph but with boxplot instead of mean and standard deviation grouping by tac and time
+
+asc_graph_boxplot <- ggplot() +
+    geom_boxplot(data = asc, aes(x = Tiempo, y = c_promedio, group = interaction(Tiempo, tac), col = tac), size = 1) +
+    # set colors
+    scale_colour_manual(values = c("#5c5c5c",  "#6c0000", "#04176b"), 
+                        labels = c("Placebo", "CAT 1", "CAT 2")) +
+    labs(x = "Time [min]", y = "Concentration [μM]", title = "Vitamin C") +
+    # change font size
+    scale_x_continuous(breaks = seq(0, 180, 30)) +
+    theme_bw()+
+    theme(legend.position = "bottom", legend.title = element_blank(), 
+        legend.text = element_text(size=8),
+        axis.text = element_text(size = 8),
+        plot.title = element_text(size = 12))
+asc_graph_boxplot
 
 # Plot mean and standard deviation for NAC c_promedio over Tiempo grouped by tac1, tac2 and p
 nac_graph <- ggplot() +
@@ -39,10 +56,11 @@ nac_graph <- ggplot() +
     stat_summary(data = nac, aes(x = Tiempo, y = c_promedio, col = tac), fun.y = mean, geom = "line", size = 1) +
     stat_summary(data = nac, aes(x = Tiempo, y = c_promedio, col = tac, shape = tac), fun.y = mean, geom = "point", size = 3) +
     stat_summary(data = nac, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, fun.args=list(mult=1), geom = "errorbar", width = 6) +
+    scale_x_continuous(breaks = seq(0, 180, 30)) +
     # set colors
-    scale_color_manual(values = c("#6c0000", "#04176b"), labels = c("Placebo", "CAT 1", "CAT 2")) +
+    scale_color_manual(values = c("#6c0000", "#04176b"), labels = c("CAT 1", "CAT 2")) +
         scale_shape_manual(values = c(16, 15),
-                        labels = c("Placebo", "CAT 1", "CAT 2")) +
+                        labels = c("CAT 1", "CAT 2")) +
     labs(x = "Time [min]", y = "Concentration [μM]", title = "N-Acetylcysteine") +
     theme_bw()+
     theme(legend.position = "bottom", legend.title = element_blank(), 
@@ -50,6 +68,23 @@ nac_graph <- ggplot() +
         axis.text = element_text(size = 8),
         plot.title = element_text(size = 12))
 nac_graph
+
+#box plot
+nac_graph_boxplot <- ggplot() +
+    geom_boxplot(data = nac, aes(x = Tiempo, y = c_promedio, group = interaction(Tiempo, tac), col = tac), size = 1) +
+    # set colors
+    scale_colour_manual(values = c("#6c0000", "#04176b"), 
+                        labels = c("CAT 1", "CAT 2")) +
+    labs(x = "Time [min]", y = "Concentration [μM]", title = "N-Acetylcysteine") +
+    # change font size
+    scale_x_continuous(breaks = seq(0, 180, 30)) +
+
+    theme_bw()+
+    theme(legend.position = "bottom", legend.title = element_blank(), 
+        legend.text = element_text(size=8),
+        axis.text = element_text(size = 8),
+        plot.title = element_text(size = 12))
+nac_graph_boxplot
 
 # Plot mean and standard deviation for DFO c_promedio over Tiempo grouped by tac1, tac2 and p
 dfo_graph <- ggplot() +
@@ -59,6 +94,8 @@ dfo_graph <- ggplot() +
     stat_summary(data = dfo, aes(x = Tiempo, y = c_promedio, col = tac, shape = tac), fun.y = mean, geom = "point", size = 3) +
     stat_summary(data = dfo, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, fun.args = list(mult=1), geom = "errorbar", width = 6) +
     # set colors
+    scale_x_continuous(breaks = seq(0, 180, 30)) +
+
     scale_color_manual(values = c("#6c0000", "#04176b"), labels = c("CAT 1", "CAT 2")) +
     scale_shape_manual(values = c(16, 15),
                         labels = c("CAT 1", "CAT 2")) +
@@ -69,6 +106,23 @@ dfo_graph <- ggplot() +
         axis.text = element_text(size = 8),
         plot.title = element_text(size = 12))
 dfo_graph
+
+#box plot
+dfo_graph_boxplot <- ggplot() +
+    geom_boxplot(data = dfo, aes(x = Tiempo, y = c_promedio, group = interaction(Tiempo, tac), col = tac), size = 1) +
+    # set colors
+    scale_colour_manual(values = c("#6c0000", "#04176b"), 
+                        labels = c("CAT 1", "CAT 2")) +
+    labs(x = "Time [min]", y = "Concentration [μM]", title = "Deferoxamine") +
+    # change font size
+    scale_x_continuous(breaks = seq(0, 180, 30)) +
+
+    theme_bw()+
+    theme(legend.position = "bottom", legend.title = element_blank(), 
+        legend.text = element_text(size=8),
+        axis.text = element_text(size = 8),
+        plot.title = element_text(size = 12))
+dfo_graph_boxplot
 
 # facet with the three graphs
 # install.packages("gridExtra")
@@ -81,24 +135,11 @@ all_plots
 # save the grid.arrange plots
 ggsave("output/concentraciones.png", all_plots, width = 12.5, height = 4, dpi = 1000)
 
+all_boxplots <- grid.arrange(asc_graph_boxplot, nac_graph_boxplot, dfo_graph_boxplot, ncol = 3)
+all_boxplots_vert <- grid.arrange(asc_graph_boxplot, nac_graph_boxplot, dfo_graph_boxplot, nrow = 3)
+ggplot(all_boxplots)
 
+# save the grid.arrange plots
+ggsave("output/concentraciones_boxplot.png", all_boxplots, width = 12.5, height = 4, dpi = 1000)
+ggsave("output/concentraciones_boxplot_vert.png", all_boxplots_vert, width = 7.5, height = 10, dpi = 1000)
 
-
-
-
-ggplot()+
-  geom_line(data = datos_nac, aes(x=t_min, y=concentration, col = patient),size = 1, alpha = 0.5, linetype = "solid")+
-  geom_boxplot(data = datos_nac, aes(x=t_min, y=concentration, group = t_min),size = 1, alpha = 0.5, col = "#00000090")+
-  # geom_boxplot(data = simulacion_ambas_sol_nac, aes(x=tiempo_continuo, y=concentracion*1000, group = tiempo_continuo),size = 1, fill = "#0000003e", col = "#772929b8")+
-  geom_line( data =  simulacion_ambas_sol_nac, aes(x=tiempo_continuo, y=concentracion*1000, color = paciente),size = 0.7)+
-  # geom_line(data = datos_nac, aes(x=t_min, y=concentration, col = patient),size = 1, linetype = "dashed")+
-  geom_point(data = datos_nac, aes(x=t_min, y=concentration, col = patient),size = 4, alpha = 0.5)+
-  # geom_line(data = gpt_hat, aes(x=tiempo, y=concentracion),size = 0.7, col="#113b19")+
-  scale_x_continuous(breaks = seq(0, 180, 30))+
-  facet_grid(cols = vars(sol))+
-  # scale_color_viridis_d()+
-  guides(title=NULL)+
-  theme_bw()+
-  theme(legend.position = "bottom", legend.title = element_blank(), 
-        legend.text = element_text(size=8),
-        axis.text = element_text(size = 8))
