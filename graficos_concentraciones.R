949b92796fd4924514aba9b54ed0b2e0b6e999e6
dsm122n <90348806+dsm122n@users.noreply.github.com>
2023-05-06 03:20:57 +0000

    graficos corregidos

--- graficos_concentraciones.R
+++ graficos_concentraciones.R
@@ -11,30 +11,39 @@ dfo <- tibble(read.csv("output/covariables_dfo.csv", header = TRUE, sep = ","))
 # Plot mean and standard deviation for ASC c_promedio over Tiempo grouped by tac1, tac2 and p
 asc_graph <- ggplot() +
     # add mean and standard deviation
-    stat_summary(data = asc, aes(x = Tiempo, y = c_promedio, col = tac, linetype = tac), fun.y = mean, geom = "line", size = 1) +
-    stat_summary(data = asc, aes(x = Tiempo, y = c_promedio, col = tac), fun.y = mean, geom = "point", size = 3) +
-    stat_summary(data = asc, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, geom = "errorbar", width = 0.1) +
+    stat_summary(data = asc, aes(x = Tiempo, y = c_promedio, col = tac), fun.y = mean, geom = "line", size = 1) +
+    stat_summary(data = asc, aes(x = Tiempo, y = c_promedio, col = tac, shape = tac), fun.y = mean, geom = "point", size = 3) +
+    stat_summary(data = asc, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, fun.args=list(mult=1), geom = "errorbar", width = 6) +
+    #stat_summary(data = asc, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, fun.args=list(mult=sqrt(length(unique(asc$px)))), geom = "errorbar", width = 6) +
+    # if you want to add standard deviation adjusted to 95% confidence interval
+    # stat_summary(data = asc, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, fun.args=list(mult=1.96), geom = "errorbar", width = 4) +
     # set colors
-    scale_color_manual(values = c("#5c5c5c",  "#6c0000", "#04176b")) +
+    scale_color_manual(values = c("#5c5c5c",  "#6c0000", "#04176b"), 
+                        labels = c("Placebo", "CAT 1", "CAT 2")) +
+    scale_shape_manual(values = c(17, 16, 15),
+                        labels = c("Placebo", "CAT 1", "CAT 2")) +
     labs(x = "Time [min]", y = "Concentration [μM]", title = "Vitamin C") +
     # change font size
     theme_bw()+
     theme(legend.position = "bottom", legend.title = element_blank(), 
         legend.text = element_text(size=8),
         axis.text = element_text(size = 8),
-
         plot.title = element_text(size = 12))
 asc_graph
+
+
 # Plot mean and standard deviation for NAC c_promedio over Tiempo grouped by tac1, tac2 and p
 nac_graph <- ggplot() +
     # geom_point(data = nac, aes(x = Tiempo, y = c_promedio, col = tac)) +
     # add mean and standard deviation
     stat_summary(data = nac, aes(x = Tiempo, y = c_promedio, col = tac), fun.y = mean, geom = "line", size = 1) +
-    stat_summary(data = nac, aes(x = Tiempo, y = c_promedio, col = tac), fun.y = mean, geom = "point", size = 3) +
-    stat_summary(data = nac, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, geom = "errorbar", width = 0.1) +
+    stat_summary(data = nac, aes(x = Tiempo, y = c_promedio, col = tac, shape = tac), fun.y = mean, geom = "point", size = 3) +
+    stat_summary(data = nac, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, fun.args=list(mult=1), geom = "errorbar", width = 6) +
     # set colors
-    scale_color_manual(values = c("#6c0000", "#04176b")) +
-    labs(x = "Time [min]", y = "Concentration [μM]", title = "N-Acetyl-Cysteine") +
+    scale_color_manual(values = c("#6c0000", "#04176b"), labels = c("Placebo", "CAT 1", "CAT 2")) +
+        scale_shape_manual(values = c(16, 15),
+                        labels = c("Placebo", "CAT 1", "CAT 2")) +
+    labs(x = "Time [min]", y = "Concentration [μM]", title = "N-Acetylcysteine") +
     theme_bw()+
     theme(legend.position = "bottom", legend.title = element_blank(), 
         legend.text = element_text(size=8),
@@ -47,10 +56,12 @@ dfo_graph <- ggplot() +
     # geom_point(data = dfo, aes(x = Tiempo, y = c_promedio, col = tac)) +
     # add mean and standard deviation
     stat_summary(data = dfo, aes(x = Tiempo, y = c_promedio, col = tac), fun.y = mean, geom = "line", size = 1) +
-    stat_summary(data = dfo, aes(x = Tiempo, y = c_promedio, col = tac), fun.y = mean, geom = "point", size = 3) +
-    stat_summary(data = dfo, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, geom = "errorbar", width = 0.1) +
+    stat_summary(data = dfo, aes(x = Tiempo, y = c_promedio, col = tac, shape = tac), fun.y = mean, geom = "point", size = 3) +
+    stat_summary(data = dfo, aes(x = Tiempo, y = c_promedio, col = tac), fun.data = mean_se, fun.args = list(mult=1), geom = "errorbar", width = 6) +
     # set colors
-    scale_color_manual(values = c("#6c0000", "#04176b")) +
+    scale_color_manual(values = c("#6c0000", "#04176b"), labels = c("CAT 1", "CAT 2")) +
+    scale_shape_manual(values = c(16, 15),
+                        labels = c("CAT 1", "CAT 2")) +
     labs(x = "Time [min]", y = "Concentration [μM]", title = "Deferoxamine") +
     theme_bw()+
     theme(legend.position = "bottom", legend.title = element_blank(), 
@@ -69,17 +80,6 @@ all_plots <- all_plots + theme(text = element_text(size=12))
 all_plots
 # save the grid.arrange plots
 ggsave("output/concentraciones.png", all_plots, width = 12.5, height = 4, dpi = 1000)
-
-# testing ggplot
-tibble <- tibble(x = c(1,2,3,4,5), y = c(1,2,3,4,5))
-ggplot(tibble, aes(x = x, y = y)) + geom_point()
-
-
-
-
-
-
-
 
 
 
