65191eff8ea2d6a36c2e2ce57e7d7b5342344b72
dsm122n <dsm122n@gmail.com>
2023-08-13 15:37:04 +0000

    summary

---
+++ output/non_comp_analysis/summary.R
@@ -0,0 +1,9 @@
+library(dplyr)
+library(ggplot2)
+library(tibble)
+
+# read data
+
+asc_nca <- read.csv("output/non_comp_analysis/asc_nca.csv")
+nac_nca <- read.csv("output/non_comp_analysis/nac_nca.csv")
+dfo_nca <- read.csv("output/non_comp_analysis/dfo_nca.csv")
